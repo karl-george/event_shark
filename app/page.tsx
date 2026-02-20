7a18017219c1ac2542432d8b03433b54fd7a169d
Karl George <karlgeorge87@gmail.com>
2026-02-20 16:09:45 +0100

    Added LightRays and changed fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 const Page = () => {
     return (
-        <div>Hello Nextjs16</div>
+        <section>
+            <h1 className="text-center">The Hub for Every Dev <br/> Event You Can't Miss</h1>
+            <p className="text-center mt-5">Hackathons, Meetups and Conferences. All in One Place</p>
+        </section>
     )
 }
 export default Page
